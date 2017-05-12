fed269e8d65c3a4aecb9e6c70734e9f05c5fe0ec
@Vlad <vlad.owl4@gmail.com>
2017-05-12 10:53:29 +0300

    Add select products

--- api/controllers/ProductController.js
+++ api/controllers/ProductController.js
@@ -7,7 +7,11 @@
 
 module.exports = {
 	select: function(req, res){
-
+    Post.find(req.category)
+        .exec(function (err, products) {
+          if (err) return res.send(500);
+          res.json(products);
+        });
   }
 };
 
--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,8 @@ module.exports.routes = {
 
   '/': {
     view: 'homepage'
-  }
+  },
+  //"POST /product/select": {controller: "FooController", action: "bar"}
 
   /***************************************************************************
   *                                                                          *
