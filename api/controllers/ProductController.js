09761f6d281ac77798e217967fbf49a426c1105a
@Vlad <vlad.owl4@gmail.com>
2017-05-12 11:40:08 +0300

    Add select products

--- api/controllers/ProductController.js
+++ api/controllers/ProductController.js
@@ -12,7 +12,7 @@ module.exports = {
           if (err) return res.send(500);
           var newProd = [];
           for(i=0; i< products.length; i++){
-            if(products[i].category == req.params.category){
+            if(products[i].category.id == req.params.category){
               newProd.push(products[i]);
             }
           }
